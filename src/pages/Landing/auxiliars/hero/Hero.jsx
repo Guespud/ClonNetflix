9031aaab19b16786106130a96a6d03be23236152
Alejandro Guespud <alejo.habbacuc@gmail.com>
2021-03-18 13:22:22 -0500

    estructura proyecto

---
+++ src/pages/Landing/auxiliars/hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './hero.scss'
+
+const Hero = () => {
+    return (
+        <div className="LandingHero">
+            <img className="LandingHero_bg" src="/images/homenetflix.jpg"/>
+        </div>
+    )
+}
+
+export default Hero
---
+++ src/pages/Landing/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from './auxiliars/hero/Hero'
+import './styles.scss'
+
+const LandingPage = () => {
+    return (
+        <div className="LandingPage">
+            <Hero/>
+        </div>
+    )
+}
+
+export default LandingPage;
