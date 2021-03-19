ee30668f4349edbc26f4b80e766269d3e9ad879b
Alejandro Guespud <alejo.habbacuc@gmail.com>
2021-03-18 19:56:53 -0500

    error background

---
+++ src/pages/Landing/auxiliars/headers/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.scss';
+
+const Header = () => {
+    return (
+        <div className="LandingHeader">
+        <img className="LandingHeader_logo" src="/images/netflix-logo.eps"/>    
+
+<div className="LandingHeader_button">
+    <p>
+        Iniciar sesión
+    </p>
+</div>
+
+        </div>
+    )
+}
+
+export default Header;
--- src/pages/Landing/auxiliars/hero/Hero.jsx
+++ src/pages/Landing/auxiliars/hero/Hero.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import Header from '../headers'
 import './hero.scss'
 
 const Hero = () => {
     return (
         <div className="LandingHero">
             <img className="LandingHero_bg" src="/images/homenetflix.jpg"/>
+            <Header/>
         </div>
     )
 }
 
-export default Hero
+export default Hero;
