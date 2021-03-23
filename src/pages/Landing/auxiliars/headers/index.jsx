3a522f9daa40e0a237decb4a351ac0a1abd8a868
Alejandro Guespud <alejo.habbacuc@gmail.com>
2021-03-22 19:36:40 -0500

    segundo commit , landing principal

--- src/pages/Landing/auxiliars/headers/index.jsx
+++ src/pages/Landing/auxiliars/headers/index.jsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import './styles.scss';
+import React from "react";
+import "./styles.scss";
+import Logo from '../../../../assets/images/logon.svg'
 
 const Header = () => {
-    return (
-        <div className="LandingHeader">
-        <img className="LandingHeader_logo" src="/images/netflix-logo.eps"/>    
+  return (
+    <div className="LandingHeader">
+      <img className="LandingHeader_logo" alt="este es el logo de netflix" src={Logo} />
 
-<div className="LandingHeader_button">
-    <p>
-        Iniciar sesión
-    </p>
-</div>
-
-        </div>
-    )
-}
+      <div className="LandingHeader_button">
+        <p>Iniciar sesión</p>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
--- src/pages/Landing/auxiliars/hero/Hero.jsx
+++ src/pages/Landing/auxiliars/hero/Hero.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
-import Header from '../headers'
-import './hero.scss'
+import React from "react";
+import Header from "../headers";
+import "./hero.scss";
 
 const Hero = () => {
-    return (
-        <div className="LandingHero">
-            <img className="LandingHero_bg" src="/images/homenetflix.jpg"/>
-            <Header/>
+  return (
+    <div className="LandingHero">
+      <Header />
+      <div className="LandingHero__content">
+        <h1 className="LandingHero_content--title">
+          Películas y series <br/>ilimitadas y mucho más.
+        </h1>
+        <h3 className="LandingHero__content--subtitle">
+          Disfruta donde quieras. Cancela cuando quieras.
+        </h3>
+        <h6 className="LandingHero_content--text">
+          ¿Quieres ver Netflix ya? Ingresa tu email para crear una cuenta o
+          reiniciar tu membresía de Netflix.
+        </h6>
+      </div>
+      <div className="Email_imput">
+        <input type="text" className="Email" />
+        <div className="Button">
+          <p className="Button_text">Comenzar {">"}</p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
